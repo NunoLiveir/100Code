5fe96d3072ceda02df5906e95b901b4c47199811
NunoLiveir <138186444+NunoLiveir@users.noreply.github.com>
2023-07-02 16:11:11 +0200

    Create BNG.py
    1. Band Name Generator

---
+++ BNG.py
@@ -0,0 +1,5 @@
+
+print("Welcome to the Band Name Generator!")
+city = input("What's the name of the city where you were born? " + "\n")
+pet = input("What's the name of your pet? " + "\n")
+print("Your band name could be: " + city + " " + pet)
