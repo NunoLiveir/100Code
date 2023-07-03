7a2e508d99ab5949a80f1e55199e57eaeea5155a
NunoLiveir <138186444+NunoLiveir@users.noreply.github.com>
2023-07-03 15:17:50 +0200

    Update 2 - tip_calc.py

--- 2 - tip_calc.py
+++ 2 - tip_calc.py
@@ -4,5 +4,5 @@ percent = input("What percentage tip would you like to give? 10, 12, or 15? ")
 people = input("How many people will split the bill? ")
 tip_value = 1 + (int(percent) / 100)
 final_tip = (float(total) / int(people) * tip_value)
-rounded_tip = "{:.2f}.format{final_tip}
+rounded_tip = "{:.2f}".format(final_tip)
 print(f"Each person should pay: ${rounded_tip}")
